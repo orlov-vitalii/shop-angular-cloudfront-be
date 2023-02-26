b433e3b58bd596cc697018254c455c2d659f6d85
Vitalii Orlov <vitalii_orlov@epam.com>
2023-02-26 23:57:28 +0200

    delete unnessessary files

--- product-service/handler.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const { getProductById } = require("./handlers/getProductById");
-const { getProducts } = require("./handlers/getProducts");
-
-module.exports.getProducts = getProducts;
-module.exports.getProductById = getProductById;
