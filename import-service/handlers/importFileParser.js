88f536b1b607847b966f1037a98c7f4ca9d9ffcf
Vitalii Orlov <vitalii_orlov@epam.com>
2023-03-28 09:51:57 +0300

    task 6

--- import-service/handlers/importFileParser.js
+++ import-service/handlers/importFileParser.js
@@ -5,38 +5,43 @@ const BUCKET = 'orlov-vitalii-import';
 
 module.exports.importFileParser = async (event) => {
     const key = get(event, 'Records[0].s3.object.key');
-    console.log(JSON.stringify(event), key);
     
     const params = {
         Bucket: BUCKET,
         Key: key
     }
-
-    const s3 = new AWS.S3({ region: 'eu-west-1' });
-    const s3Stream = s3.getObject(params).createReadStream();
-
-    return await new Promise((resolve) => {
-        const results = [];
     
+    const s3 = new AWS.S3({ region: 'eu-west-1' });
+    const sqs = new AWS.SQS();
+    const s3Stream = s3.getObject(params).createReadStream();
+    
+    return await new Promise((resolve, reject) => {
+        const result = [];
+        
         s3Stream
-            .pipe(csv())
-            .on('data', (data) => {
-                console.log('DATA:', data);
-                results.push(data);
-            })
-            .on('end', () => {
-                console.log('RESULT:', results);
+        .pipe(csv())
+        .on('data', (data) => {
+            result.push(data);
+        })
+        .on('end', () => {
+            console.log('RESULT: ', result);
+            sqs.sendMessage({
+                QueueUrl: process.env.SQS_URL,
+                MessageBody: JSON.stringify(result)
+            }, (err, data) => {
+                console.log('SQS-ERROR: ', err, 'SQS: ', data)
                 resolve({
                     statusCode: 200,
-                    body: JSON.stringify({ message: 'S3 object was succesfully received and parsed.' })
+                    body: JSON.stringify({ message: 'S3 object was succesfully received, parsed and sent to SQS.' })
                 })
             })
-            .on('error', (error) => {
-                console.error(error);
-                resolve({
-                    statusCode: 500,
-                    body: JSON.stringify({ message: `Error when reading S3 stream` })
-                })
+        })
+        .on('error', (error) => {
+            console.error(error, error.errorMessage);
+            reject({
+                statusCode: 500,
+                body: JSON.stringify({ message: `Error when reading S3 stream` })
             })
+        })
     })     
 }
--- product-service/handler.js
+++ product-service/handler.js
@@ -2,8 +2,10 @@
 
 const { getProductById } = require("./handlers/getProductById");
 const { getProducts } = require("./handlers/getProducts");
-const { createProduct } = require("./handlers/createProduct"); 
+const { createProduct } = require("./handlers/createProduct");
+const { catalogBatchProcess } = require("./handlers/catalogBatchProcess");
 
 module.exports.getProducts = getProducts;
 module.exports.getProductById = getProductById;
 module.exports.createProduct = createProduct;
+module.exports.catalogBatchProcess = catalogBatchProcess;
